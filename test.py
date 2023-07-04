821719f57a3f7dc16f4b8fb4b5085f2ff0f13903
alio <aligharaghani0@gmail.com>
2023-07-04 23:51:35 +0330

    adding the  next file :)

---
+++ test.py
@@ -0,0 +1,15 @@
+import random
+arr = [ 1, 2 , 3 , 4 , 5 , 6, 7 , 8 ]
+# creating a list with elements and their respective weights
+
+box = ['al'  , 'cr' , 'fe' , 'ni' , 'co' ]
+weights = [4 , 1, 1 , 1 , 1 ]
+
+out_put = { }
+for i in arr :
+    selected_element = random.choices(box, weights=weights) [ 0 ]
+    if ( out_put) : #exsiting lable cheking
+
+
+
+print(selected_element)
